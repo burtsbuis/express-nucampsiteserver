33074f37efbf72697ecc37ab9c66e98cd4f93977
Kelvin Bui <kdbuimail@gmail.com>
2021-05-01 09:22:26 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '462830938135767',
+        clientSecret: '34655972ed7d07aab473d335896951ef'
+    }
 }
